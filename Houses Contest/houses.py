e97b8f02ab8d713f33942de00898dbda3cba9302
Scott Davis <ScDavis6@gmail.com>
2016-11-06 14:46:26 -0800

    Update
    Figuring out the data exploration strategy to use

--- Houses Contest/houses.py
+++ Houses Contest/houses.py
@@ -10,6 +10,7 @@ import pandas as pd
 import numpy as np
 import sklearn.linear_model as ln
 from sklearn.model_selection import cross_val_score
+from pandas.tools.plotting import scatter_matrix
 
 
 #Import train and test datasets
@@ -18,12 +19,18 @@ house_test = pd.read_csv('test.csv')
 
 
 #Data exploration
+#Name of variables
+house_train.columns
 #Dimension of dataset
 house_train.shape
 #Column datatypes
 house_train.dtypes
+#Number of numeric variables
+house_train.select_dtypes(include = ['int']).columns.values
+#Number of categorical variables
+house_train.select_dtypes(include = ['object']).columns.values
 #Number of NaN values in each column
-house_train.notnull().sum()
+house_train.isnull().sum()
 
 #Summary statistics
 #Numeric variables
@@ -32,9 +39,53 @@ house_train.describe()
 categorical = house_train.dtypes[house_train.dtypes == "object"].index
 house_train[categorical].describe()
 
-#Data preprocessing
 #Drop ID variable
 house_new = house_train.drop('Id', 1)
+
+
+
+
+#Visualizations
+
+#Step 2
+#Univariate data exploration
+#Histogram of dependent variable
+house_new['SalePrice'].hist(bins = 50)
+
+#Scatterplot matrix of numeric variables
+scatter_matrix(house_new, alpha=0.2, figsize=(6, 6))
+
+#Barcharts of all categorical variables
+#Types of dwellings
+a = house_new['MSSubClass'].value_counts()
+a.plot(kind = 'bar')
+
+#Zoning classification
+b = house_new['MSZoning'].value_counts()
+b.plot(kind = 'bar')
+
+
+
+
+
+
+
+#Step 3
+#Bi-variate data exploration - there needs to be a reason why
+#Correlations
+#Diagonal correlation matrix
+seaborn.corrplot(house_new, annot = False, diag_names = False)
+
+#Get top 10 correlations with the price variable
+
+#Get top correlations with variables to each other 
+
+#Do saleprice with OverallQual
+#Saleprice with YearBuilt
+#Saleprprice with GrLivArea
+#
+
+#Step 4
 #Data imputation
 #Impute numerical missing values with average
 house_na = house_new.fillna(house_new.mean())
@@ -42,20 +93,11 @@ house_na = house_new.fillna(house_new.mean())
 house = house_na.apply(lambda x:x.fillna(x.value_counts().index[0]))
 
 
+#Step 5 outlier detection
 
-#check for NaN values
-house.notnull().sum()
+#Step 6 variable transformation
 #Convert categorical datatype to dummy variables in dataframe
 house_final = pd.get_dummies(house)
-
-
-#Visualizations
-#Diagonal correlation matrix
-seaborn.corrplot(house, annot = False, diag_names = False)
-
-#Histogram of dependent variable
-house['SalePrice'].hist(bins = 50)
-
 
 
 #Model building
